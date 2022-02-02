44a834d89a1ca93adba6e65f433e881293a0111e
Mateusz Kowalewski <aspoka1@gmail.com>
2022-02-02 21:16:13 +0100

    Init Embedded Entity project

---
+++ EmbeddedEntity/src/main/java/pl/mkowalewski/embeddedentity/EmbeddedentityApplication.java
@@ -0,0 +1,13 @@
+package pl.mkowalewski.embeddedentity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmbeddedentityApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(EmbeddedentityApplication.class, args);
+  }
+
+}
